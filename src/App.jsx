ad6dc64dbd4d577c8c90c51b9d006ecaf3bc21ac
Rudyxclix <karthikvava009@gmail.com>
2025-01-26 16:01:05 +0530

    New Generes Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar/NavBar"
 import './App.css'
 import Banner from "./components/Banner/Banner";
 import RowPost from "./components/RowPost/RowPost";
-import { action, horror, originals, romantic, trending } from "./urls";
+import { action, comedy, documentries, horror, originals, romantic, trending } from "./urls";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
 
 function App() {
@@ -27,6 +27,9 @@ function App() {
             <RowPost url={action} title={'Action'} isSmall />
             <RowPost url={trending} title={'Trending'} isSmall />
             <RowPost url={romantic} title={'Romantic'} isSmall />
+            <RowPost url={horror} title={'Horror'} isSmall />
+            <RowPost url={documentries} title={'Documentries'} isSmall />
+            <RowPost url={comedy} title={'Comedy'} isSmall />
           </>
       }
 
--- src/urls.js
+++ src/urls.js
@@ -5,3 +5,5 @@ export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
 export const trending = `trending/all/week?api_key=${API_KEY}&language=en-US`
 export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
 export const romantic = `discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const documentries = `discover/movie?api_key=${API_KEY}&with_genres=99`
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
