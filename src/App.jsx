617a8b9ab147532f81a21d5a5c64cb91fd280602
Rudyxclix <karthikvava009@gmail.com>
2025-01-26 15:51:30 +0530

    Intro animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar"
 import './App.css'
 import Banner from "./components/Banner/Banner";
 import RowPost from "./components/RowPost/RowPost";
 import { action, horror, originals, romantic, trending } from "./urls";
+import SplashScreen from "./components/SplashScreen/SplashScreen";
 
 function App() {
 
+  const [showSplash, setShowSplash] = useState(true)
+
+  const handleSplashComplete = () => {
+    setShowSplash(false)
+  }
+
   return (
     <>
-      <NavBar />
-      <Banner />
-      <RowPost url={originals} title={'Netflix Originals'} isBackDrop />
-      <RowPost url={action} title={'Action'} isSmall />
-      <RowPost url={trending} title={'Trending'} isSmall />
-      <RowPost url={romantic} title={'Romantic'} isSmall />
+      {
+        showSplash ? 
+          <SplashScreen onComplete={handleSplashComplete} />
+          :
+          <>
+            <NavBar />
+            <Banner />
+            <RowPost url={originals} title={'Netflix Originals'} isBackDrop />
+            <RowPost url={action} title={'Action'} isSmall />
+            <RowPost url={trending} title={'Trending'} isSmall />
+            <RowPost url={romantic} title={'Romantic'} isSmall />
+          </>
+      }
+
     </>
+
   )
 }
 
